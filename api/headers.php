433b074e3bf2defe66878a1705f9567c659b0b77
Mel Macario <xmelmacario@gmail.com>
2024-03-17 18:34:24 +0800

    added addContact function

--- api/headers.php
+++ api/headers.php
@@ -1,8 +1,6 @@
 <?php
-
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Method: GET, POST, OPTIONS");
-header("Access-Control-Allow-Method: Content-Type, Authorization");
-
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 ?>
